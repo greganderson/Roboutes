d25d5d41fd66057189c139a98fa81ed22842632b
Greg Anderson <greg@infobytes.com>
2014-03-18 17:51:18 -0600

    Added drive terminal
    All drive terminal projects and resources.

---
+++ DriveTerminal/Deadzone/ToolboxControl.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Deadzone {
+    /// <summary>
+    /// Interaction logic for ToolboxControl.xaml
+    /// </summary>
+    [ProvideToolboxControl("Deadzone", true)]
+    public partial class ToolboxControl : UserControl {
+
+		/// <summary>
+		/// Returns the value of the minimum slider bar.
+		/// </summary>
+        public int Min {
+            get {
+                return Int32.Parse(minValue.Content.ToString());
+            }
+            private set {
+                minValue.Content = value;
+            }
+        }
+
+		/// <summary>
+		/// Returns the value of the maximum slider bar.
+		/// </summary>
+        public int Max {
+            get {
+                return Int32.Parse(maxValue.Content.ToString());
+            }
+            private set {
+                maxValue.Content = value;
+            }
+        }
+
+		/// <summary>
+		/// Returns the value of the sensitivity slider bar.
+		/// </summary>
+        public int Sensitivity {
+            get {
+                return Int32.Parse(sensitivityValue.Content.ToString());
+            }
+            private set {
+                sensitivityValue.Content = value;
+            }
+        }
+
+        public ToolboxControl() {
+            InitializeComponent();
+
+            minSlider.Value = 0;
+            maxSlider.Value = 0;
+            sensitivitySlider.Value = 0;
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            Min = (int)e.NewValue;
+        }
+
+        private void maxSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            Max = (int)e.NewValue;
+        }
+
+        private void sensitivitySlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            Sensitivity = (int)e.NewValue;
+        }
+    }
+}
---
+++ DriveTerminal/Inclinometer/Inclinometer/ToolboxControl.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Inclinometer {
+    /// <summary>
+    /// Interaction logic for ToolboxControl.xaml
+    /// </summary>
+    [ProvideToolboxControl("Inclinometer", true)]
+    public partial class ToolboxControl : UserControl {
+
+        private bool rollLight;
+		/// <summary>
+		/// Returns true if the roll warning light is on, false otherwise.  Sets
+        /// the light to on or off.
+		/// </summary>
+        public bool RollWarningLight {
+            get {
+                return rollLight;
+            }
+            set {
+                if (value) {
+					rollIndicatorOff.Visibility = System.Windows.Visibility.Hidden;
+					rollIndicatorOn.Visibility = System.Windows.Visibility.Visible;
+                    rollLight = true;
+                }
+                else {
+					rollIndicatorOff.Visibility = System.Windows.Visibility.Visible;
+					rollIndicatorOn.Visibility = System.Windows.Visibility.Hidden;
+                    rollLight = false;
+                }
+            }
+        }
+
+        private bool pitchLight;
+		/// <summary>
+		/// Returns true if the pitch warning light is on, false otherwise.  Sets
+        /// the light to on or off.
+		/// </summary>
+        public bool PitchWarningLight {
+            get {
+                return pitchLight;
+            }
+            set {
+                if (value) {
+					pitchIndicatorOff.Visibility = System.Windows.Visibility.Hidden;
+					pitchIndicatorOn.Visibility = System.Windows.Visibility.Visible;
+                    pitchLight = true;
+                }
+                else {
+					pitchIndicatorOff.Visibility = System.Windows.Visibility.Visible;
+					pitchIndicatorOn.Visibility = System.Windows.Visibility.Hidden;
+                    pitchLight = false;
+                }
+            }
+        }
+
+        public ToolboxControl() {
+            InitializeComponent();
+
+            pitchIndicatorOff.Visibility = System.Windows.Visibility.Visible;
+            pitchIndicatorOn.Visibility = System.Windows.Visibility.Hidden;
+            rollIndicatorOff.Visibility = System.Windows.Visibility.Visible;
+            rollIndicatorOn.Visibility = System.Windows.Visibility.Hidden;
+
+            rollLight = false;
+            pitchLight = false;
+        }
+    }
+}
