35a84696c75c80365f35fe9b97dda8de0f8b636d
matt17825 <matt17825@hotmail.com>
2014-05-22 12:48:11 -0600

    cam test

--- Video-Networking/manyCamTest/manyCamTest/MainWindow.xaml.cs
+++ Video-Networking/manyCamTest/manyCamTest/MainWindow.xaml.cs
@@ -16,6 +16,9 @@ using System.Windows.Shapes;
 using AForge.Video;
 using AForge.Video.DirectShow;
 using AForge.Controls;
+using System.Threading;
+using System.IO;
+using System.Drawing;
 
 namespace manyCamTest
 {
@@ -24,40 +27,112 @@ namespace manyCamTest
     /// </summary>
     public partial class MainWindow : Window
     {
-        VideoSourcePlayer cam1 = new VideoSourcePlayer();
-        //VideoSourcePlayer cam2 = new VideoSourcePlayer();
-       // VideoSourcePlayer cam3 = new VideoSourcePlayer();
+        WPFBitmapConverter converter;
+        string[] camIds;
 
         public MainWindow()
         {
             InitializeComponent();
+            converter = new WPFBitmapConverter();
 
             ////////////////////Video Config - Start//////////////////////
             FilterInfoCollection videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
-            //string toPrint = "";
-            int camCount = 0;
+
+            camIds = new string[videoDevices.Count];
+            int count = -1;
             foreach (FilterInfo vidDevice in videoDevices)
             {
-                //toPrint += vidDevice.MonikerString + "  \n  ";
-                camCount++;
+                count++;
+                camIds[count] = vidDevice.MonikerString;
+                VideoCaptureDevice temp = new VideoCaptureDevice(vidDevice.MonikerString);
+                temp.Start();
+                temp.NewFrame += NewFrame;
             }
-            MessageBox.Show("Found: " + camCount + " cameras");
+            MessageBox.Show("Found: " + videoDevices.Count + " cameras");
+        }
 
-            VideoCaptureDevice cam1Source = new VideoCaptureDevice(videoDevices[0].MonikerString);
-          //  VideoCaptureDevice cam2Source = new VideoCaptureDevice(videoDevices[1].MonikerString);
-           // VideoCaptureDevice cam3Source = new VideoCaptureDevice(videoDevices[2].MonikerString);
+        void NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bmp = eventArgs.Frame;
+            int index = Array.IndexOf(camIds, ((VideoCaptureDevice)sender).Source);
+            switch ( index )
+            {
+                case 0:
+                    Dispatcher.Invoke(() => cam1Image.Source = (ImageSource)converter.Convert(bmp, null, null, null));
+                    break;
+                case 1:
+                    Dispatcher.Invoke(() => cam2Image.Source = (ImageSource)converter.Convert(bmp, null, null, null));
+                    break;
+                case 2:
+                    Dispatcher.Invoke(() => cam3Image.Source = (ImageSource)converter.Convert(bmp, null, null, null));
+                    break;
+                case 3:
+                    Dispatcher.Invoke(() => cam4Image.Source = (ImageSource)converter.Convert(bmp, null, null, null));
+                    break;
+                case 4:
+                    Dispatcher.Invoke(() => cam5Image.Source = (ImageSource)converter.Convert(bmp, null, null, null));
+                    break;
+                case 5:
+                    Dispatcher.Invoke(() => cam6Image.Source = (ImageSource)converter.Convert(bmp, null, null, null));
+                    break;
+                case 6:
+                    Dispatcher.Invoke(() => cam7Image.Source = (ImageSource)converter.Convert(bmp, null, null, null));
+                    break;
+                case 7:
+                    Dispatcher.Invoke(() => cam8Image.Source = (ImageSource)converter.Convert(bmp, null, null, null));
+                    break;
+                case 8:
+                    Dispatcher.Invoke(() => cam9Image.Source = (ImageSource)converter.Convert(bmp, null, null, null));
+                    break;
+            }
+        }
 
-            cam1.VideoSource = cam1Source;
-           // cam2.VideoSource = cam2Source;
-           // cam3.VideoSource = cam3Source;
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
 
-            cam1Viewer.Child = cam1;
-           // cam2Viewer.Child = cam2;
-           // cam3Viewer.Child = cam3;
+    public class WPFBitmapConverter : IValueConverter
+    {
+        #region IValueConverter Members
 
-            cam1.Start();
-           // cam2.Start();
-           // cam3.Start();
+        public object Convert(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            MemoryStream ms = new MemoryStream();
+            ((System.Drawing.Bitmap)value).Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.EndInit();
+
+            return image;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+
+    public class ByteImageConverter
+    {
+        public static ImageSource ByteToImage(byte[] imageData)
+        {
+            BitmapImage biImg = new BitmapImage();
+            MemoryStream ms = new MemoryStream(imageData);
+            biImg.BeginInit();
+            biImg.StreamSource = ms;
+            biImg.EndInit();
+
+            ImageSource ImgSrc = biImg as ImageSource;
+
+            return ImgSrc;
         }
     }
 }
